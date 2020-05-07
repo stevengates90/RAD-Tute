708220e40dd0db8025d601d47b994d02e596d442
Steven Lee <s3674412@student.rmit.edu.au>
2020-05-07 21:37:23 +1000

    RAD-Tute

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,13 +4,14 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to user
-    else
-      flash.now[:danger] = 'Invalid email/password combanation'
-      render 'new'
+  if user && user.authenticate(params[:session][:password])
+    log_in user
+    params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+    redirect_to user
+  else
+    flash.now[:danger] = 'Invalid email/password combination'
+    # Not quite right!
+    render 'new'
     end
   end
 
@@ -18,5 +19,4 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
-
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,13 +13,12 @@ class ActiveSupport::TestCase
   def log_in_as(user)
     session[:user_id] = user.id
   end
-  # Add more helper methods to be used by all tests here...
 end
 
 class ActionDispatch::IntegrationTest
+# Log in as a particular user.
   def log_in_as(user, password: 'password', remember_me: '1')
-    post login_path, params: { session: { email: user.email,
-                                          password: password,
-                                          remember_me: remember_me} }
+    post login_path, params: {session: {email: user.email, password: password, remember_me: remember_me } }
   end
+  # Add more helper methods to be used by all tests here...
 end
