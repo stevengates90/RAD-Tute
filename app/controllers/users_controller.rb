a0a19d458ddbca060d429726a9d3494df08f1648
Steven Lee <s3674412@student.rmit.edu.au>
2020-05-28 20:46:44 +1000

    RAD-Tute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
 
   def show     
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN  
+    redirect_to root_url
   end    
   def create
     @user = User.new(user_params)
