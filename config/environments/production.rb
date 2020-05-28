05be77d298cb1a960b338469e50c0f03ecd3b4ab
Steven Lee <s3674412@student.rmit.edu.au>
2020-05-28 20:04:50 +1000

    RAD-Tute

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'polar-scrubland-72622.herokuapp.com'
+  host = 'rocky-inlet-57262.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
   resources :account_activations, only: [:edit]
   resources :users
+  resources :password_resets, only: [:new, :create, :edit, :update]
   get 'users/new'
 
   #get 'static_pages/home'
